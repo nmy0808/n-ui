44a682bd02ea808a18ae1a13e649454a0f8139ba
niemingyan <13703638210@163.com>
2021-04-05 14:18:54 +0800

    栅格系统单元测试

---
+++ tests/unit/row.spec.js
@@ -0,0 +1,29 @@
+import { shallowMount } from '@vue/test-utils';
+import Row from '@/components/layout/grid/Row.vue'
+describe('Row.vue', () => {
+  describe('props', () => {
+    let wrapper = null;
+    beforeEach(() => {
+      wrapper = shallowMount(Row);
+    })
+    it('gutter', async () => {
+      let gutter = 10;
+      await wrapper.setProps({ gutter });
+      const { marginLeft, marginRight } = getComputedStyle(wrapper.element);
+      expect(parseInt(marginLeft)).toBe(- gutter / 2);
+      expect(parseInt(marginRight)).toBe(- gutter / 2);
+    })
+    it('align', async () => {
+      let align = 'middle';
+      await wrapper.setProps({ align });
+      const { alignItems } = getComputedStyle(wrapper.element);
+      expect(alignItems).toEqual('center');
+    })
+    it('justify', async () => {
+      let justify = 'start';
+      await wrapper.setProps({ justify });
+      const { justifyContent } = getComputedStyle(wrapper.element);
+      expect(justifyContent).toEqual('start');
+    })
+  })
+})
