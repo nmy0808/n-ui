e83eef153225d6b848cfc8fd444c2a3806b4ffb0
niemingyan <1942350658@qq.com>
2021-04-19 08:06:09 +0800

    简化validate验证函数

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -10,42 +10,36 @@ export function validate(dates, rules) {
   const ruleArr = rules;
   ruleArr.forEach((rule) => {
     const { key } = rule;
-    const date = dateObj[key];
-    const { pattern } = rule;
+    const value = dateObj[key];
+    let { pattern } = rule;
     const minLength = rule.minLength || 0;
     // 验证必填 (优先级最高)
     if (rule.required) {
-      if (!date && date !== 0) {
-        error[key] = { required: true };
+      if (!value && value !== 0) {
+        error[key] = { required: '必填' };
         return;
       }
     }
     // 验证pattern
-    if (typeof pattern === 'string') {
+    if (pattern) {
       if (pattern === 'email') {
-        const reg = /.+@.+/g;
-        const regRes = reg.test(date);
-        if (!regRes) {
-          ensureObject(error, key);
-          error[key].pattern = '邮箱不合法';
-        }
+        pattern = /.+@.+/g;
       }
-    } else if (pattern && pattern.constructor.name === 'RegExp') {
-      const regRes = pattern.test(date);
+      const regRes = pattern.test(value);
       if (!regRes) {
         ensureObject(error, key);
-        error[key].pattern = '邮箱不合法';
+        error[key].pattern = '不符合规则';
       }
     }
+
     // 验证最小值
     if (minLength > 0) {
-      if (date.length < minLength) {
+      if (value.length < minLength) {
         ensureObject(error, minLength);
         error[key].minLength = '该字符串长度不合法';
       }
     }
   });
-  console.log(error);
   return error;
 }
 function ensureObject(obj, key) {
